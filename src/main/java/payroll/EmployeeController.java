623f172cadbed7fff685e5ff26af619af43517a4
BomiaoChen <bc2346@nyu.edu>
2019-05-27 01:36:46 -0400

    add responseEntity handles old and new so that more REST

--- src/main/java/payroll/EmployeeController.java
+++ src/main/java/payroll/EmployeeController.java
@@ -1,5 +1,7 @@
 package payroll;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,6 +9,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,8 +44,13 @@ public class EmployeeController {
 	}
 	
 	@PostMapping("/employees")
-	Employee newEmployee(@RequestBody Employee newEmployee) {
-		return repository.save(newEmployee);
+	ResponseEntity<?> newEmployee(@RequestBody Employee newEmployee) throws URISyntaxException {
+		
+		Resource<Employee> resource = assembler.toResource(repository.save(newEmployee));
+		
+		return ResponseEntity
+				.created(new URI(resource.getId().expand().getHref()))
+				.body(resource);
 	}
 	
 	// Single item
@@ -55,8 +63,9 @@ public class EmployeeController {
 	}
 	
 	@PutMapping("/employees/{id}")
-	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-		return repository.findById(id)
+	ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) throws URISyntaxException {
+		
+		Employee updatedEmployee = repository.findById(id)
 				.map(employee -> {
 					employee.setName(newEmployee.getName());
 					employee.setRole(newEmployee.getRole());
@@ -66,10 +75,19 @@ public class EmployeeController {
 					newEmployee.setId(id);
 					return repository.save(newEmployee);
 				});
+		
+		Resource<Employee> resource = assembler.toResource(updatedEmployee);
+		
+		return ResponseEntity
+				.created(new URI(resource.getId().expand().getHref()))
+				.body(resource);
 	}
 	
 	@DeleteMapping("/employees/{id}")
-	void deleteEmployee(@PathVariable Long id) {
+	ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
+		
 		repository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
